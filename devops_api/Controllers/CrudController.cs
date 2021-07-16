2325a6217c6ab3c22d5f6827cd7e719eea5a97e5
Astha Choudhary <87311481+choudharyastha@users.noreply.github.com>
2021-07-16 21:10:07 +0530

    dummy data added

--- devops_api/Controllers/CrudController.cs
+++ devops_api/Controllers/CrudController.cs
@@ -12,6 +12,19 @@ namespace devops_test_api.Controllers
     public class CrudController: ControllerBase
     {
         private static List<Person> persons = new List<Person>();
+        private static List<Person> persons = new List<Person> { 
+               new Person {person_id = 1, balance =  1000, name  =  "Anurag Agrawal", email_id = "anurag.agrawal@lntinfotech.com" },
+               new Person {person_id = 2, balance =  1000, name  =  "Murtuza Patel", email_id = "murtuza.patel@lntinfotech.com" },
+               new Person {person_id = 3, balance =  1000, name  =  "Vineet Bakshi", email_id = "vineet.bakshi@lntinfotech.com" },
+               new Person {person_id = 4, balance =  1000, name  =  "Astha Chaudhary", email_id = "astha.chaudhary@lntinfotech.com" }
+        };
+
+
+            //persons.Add( new Person(1, 1000, "Anurag Agrawal", "anurag.agrawal@lntinfotech.com"));
+            //persons.Add(new Person(2, 1030, "Murtuza Patel", "murtuza.patel@lntinfotech.com"));
+            //persons.Add(new Person(3, 1200, "Vineet Bakshi", "vineet.bakshi@lntinfotech.com"));
+            //persons.Add(new Person(4, 1400, "Astha Chaudhary", "astha.chaudhary@lntinfotech.com"));
+
 
         [HttpPost]
         public ActionResult<Person> AddUser(Person person)
