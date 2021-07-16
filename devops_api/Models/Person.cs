9a1b9c5efb8828ec200c5126106cde2c22c53739
Astha Choudhary <87311481+choudharyastha@users.noreply.github.com>
2021-07-16 21:11:53 +0530

    Update Person.cs

--- devops_api/Models/Person.cs
+++ devops_api/Models/Person.cs
@@ -7,6 +7,17 @@ namespace devops_test_api.Models
 {
     public class Person
     {
+        public Person()
+        {
+
+        }
+         public Person(int person_id, int balance, string name, string email_id)
+        {
+            this.person_id = person_id;
+            this.balance = balance;
+            this.name = name;
+            this.email_id = email_id;
+        }
         public int person_id { get; set; }
         public int balance { get; set; }
         public string name { get; set; }
